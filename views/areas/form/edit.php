f1f2983228ad67192e3406b67a9881bca55fd053
Christoph Wiest <cwiest@intera.de>
2017-11-30 14:20:53 +0100

    [TASK] edit formbuilder edit block
    Relates: #14322

--- views/areas/form/edit.php
+++ views/areas/form/edit.php
@@ -1,29 +1,22 @@
 <div class="configWindow">
-
-    <div class="--row">
-
         <div class="col-xs-12">
             <div class="form-group">
                 <label for="form"><?= $this->translateAdmin('form') ?></label><br>
-                <?= $this->select('formName', ['width' => '600', 'class' => 'form-control', 'placeholder' => $this->translateAdmin('form'), 'id' => 'formName', 'store' => $this->availableForms]) ?>
+                <?= $this->select('formName', ['width' => '450px', 'class' => 'form-control', 'placeholder' => $this->translateAdmin('form'), 'id' => 'formName', 'store' => $this->availableForms]) ?>
             </div>
         </div>
 
         <div class="col-xs-12">
             <div class="form-group">
-                <label for="responseTemplate"><?= $this->translateAdmin('response template') ?></label><br>
-                <?= $this->href('responseTemplate', ['width' => '600', 'class' => 'form-control', 'id' => 'responseTemplate']) ?>
+                <label for="responseTemplate"><?= $this->translateAdmin('Antwort Vorlage') ?></label><br>
+                <?= $this->href('responseTemplate', ['width' => '450px', 'class' => 'form-control', 'id' => 'responseTemplate']) ?>
             </div>
         </div>
 
         <div class="col-xs-12">
             <div class="form-group">
                 <label for="sendMailTemplate"><?= $this->translateAdmin('mail template') ?></label><br>
-                <?= $this->href('sendMailTemplate', ['width' => '600', 'class' => 'form-control', 'id' => 'sendMailTemplate']) ?>
+                <?= $this->href('sendMailTemplate', ['width' => '450px', 'class' => 'form-control', 'id' => 'sendMailTemplate']) ?>
             </div>
         </div>
-
-
-    </div>
-
 </div>
